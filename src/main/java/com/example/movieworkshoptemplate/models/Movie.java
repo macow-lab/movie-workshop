0a4f764360537ee5e48058b1d7ebfb7cbdf4a11b
iqraabbasi <iqra_abbasi2000@hotmail.dk>
2021-09-27 09:56:41 +0200

    constructor

--- src/main/java/com/example/movieworkshoptemplate/models/Movie.java
+++ src/main/java/com/example/movieworkshoptemplate/models/Movie.java
@@ -2,4 +2,22 @@ package com.example.movieworkshoptemplate.models;
 
 //Movie POJO
 public class Movie {
+
+    //3.2
+    int year;
+    int length;
+    String title;
+    String subject;
+    int popularity;
+    boolean awards;
+
+    public Movie(int year, int length, String title, String subject, int popularity, boolean awards) {
+        this.year = year;
+        this.length = length;
+        this.title = title;
+        this.subject = subject;
+        this.popularity = popularity;
+        this.awards = awards;
+
+    }
 }
