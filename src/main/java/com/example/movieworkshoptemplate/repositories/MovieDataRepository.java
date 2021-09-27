78fb0abbb9372502cf8031e053dcf869889b064a
macow-lab <mmacovv@gmail.com>
2021-09-27 11:18:40 +0200

    Exercise 1 and 2 complete

--- src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
+++ src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
@@ -1,32 +1,38 @@
 package com.example.movieworkshoptemplate.repositories;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import com.example.movieworkshoptemplate.models.Movie;
 import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
 
 public class MovieDataRepository {
     // Methods to get data from file
 
     public MovieDataRepository() {
         readFile();
+        System.out.println(movieRepository.size());
     }
 
     ArrayList<Movie> movieRepository = new ArrayList<>();
 
     public void readFile() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("resources/imdb-data.csv"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] values = line.split(",");
+        File f = new File("resources/imdb-data.csv");
+        Scanner sc;
+        try {
+            sc = new Scanner(f);
+            while (sc.hasNextLine()) {
+                if(movieRepository.size() == 0) {
+                    sc.nextLine();
+                }
+                String line = sc.nextLine();
+                String[] values = line.split(";");
                 movieRepository.add(new Movie(Integer.parseInt(values[0]), Integer.parseInt(values[1]), values[2], values[3], Integer.parseInt(values[4]), values[5]));
             }
         } catch (Exception e) {
             System.out.println("Error while reading file");
         }
+        System.out.println("File reading complete!");
     }
 
     public String getFirst() {
@@ -35,8 +41,8 @@ public class MovieDataRepository {
 
     }
     public String getRandom() {
-        Random random = new Random ();
-        Movie m  = movieRepository.get(random.nextInt(movieRepository.size()));
+        Random random = new Random();
+        Movie m  = movieRepository.get(random.nextInt(movieRepository.size() -  1));
         return m.getTitle();
     }
 }
