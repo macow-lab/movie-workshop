00f54eeba0043903a92ef8b6627eb211588baacb
macow-lab <mmacovv@gmail.com>
2021-09-27 10:10:29 +0200

    Progress on IO on data file

--- src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
+++ src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import com.example.movieworkshoptemplate.models.Movie;
 
 public class MovieDataRepository {
     // Methods to get data from file
@@ -15,7 +16,7 @@ public class MovieDataRepository {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split(",");
-                movieRepository.add("Added file");
+                movieRepository.add(new Movie(Integer.parseInt(values[0]), Integer.parseInt(values[1]), values[2], values[3], Integer.parseInt(values[4]), values[5]));
             }
         } catch (Exception e) {
             System.out.println("Error while reading file");
